cccbcbc17a5752b5a4356391d1100889d1f59a1b
ffw.georgi.donchev <georgi.donchev@ffw.com>
2025-04-04 16:29:07 +0300

    Adding ex. 5 and 6

--- 6. Asynchronous Programming - Exercise/05. Accordion/accordion.js
+++ 6. Asynchronous Programming - Exercise/05. Accordion/accordion.js
@@ -1,3 +1,70 @@
-function solution() {
-    //TODO .....
+async function solution() {
+    let endpoints = {
+        getAllArticles: 'http://localhost:3030/jsonstore/advanced/articles/list',
+        getCurrentArticle: 'http://localhost:3030/jsonstore/advanced/articles/details/'
+    }
+
+    let mainElement = document.getElementById('main');
+
+    let allArticles = await getAllArticles()
+    
+
+    async function getAllArticles() {
+        let responce = await fetch(endpoints.getAllArticles);
+        let data = await responce.json();
+        return data
+    }
+
+    allArticles.forEach(article => {
+        let {_id, title} = article
+
+        let div = createDiv(_id, title);
+        mainElement.appendChild(div);
+        
+    });
+    
+    function createDiv(_id, title) {
+        
+        let div = document.createElement('div');
+        div.classList.add('accordion');
+        div.innerHTML = `
+            <div class="head">
+                <span>${title}</span>
+                <button class="button" id="${_id}">More</button>
+            </div>
+            <div class="extra">
+                <p>Scalable Vector Graphics .....</p>
+            </div>
+        `;
+
+        div.querySelector('button')
+        .addEventListener('click', function (e) {getMoreInfo(_id, e)});
+        return div
+    }
+
+    async function getMoreInfo(_id, e) {
+        let currentDiv = document.getElementById(_id).parentElement.parentElement;
+        let button = currentDiv.querySelector('button')
+        let extraDiv = currentDiv.querySelector('.extra');
+
+        let url = endpoints.getCurrentArticle + _id;
+
+        let responce = await fetch(url);
+        let data = await responce.json()
+        
+        extraDiv.firstElementChild.textContent = data.content;
+        switch (button.textContent) {
+            case 'More':
+                extraDiv.style.display = 'inline';
+                button.textContent = 'Less'
+            break;
+
+            case 'Less':
+                extraDiv.style.display = 'none';
+                button.textContent = 'More'
+            break;
+        }
+    }
 }
+
+solution()
--- 6. Asynchronous Programming - Exercise/06.Blog/app.js
+++ 6. Asynchronous Programming - Exercise/06.Blog/app.js
@@ -1,5 +1,101 @@
 function attachEvents() {
-    console.log('TODO...');
+    baseURL = 'http://localhost:3030/';
+    let relativePaths = {
+        posts: 'jsonstore/blog/posts/',
+        comments: 'jsonstore/blog/comments/'
+    }
+    let endpoints = {
+        posts: baseURL + relativePaths.posts,
+        comments: baseURL + relativePaths.comments
+    }
+
+    let loadPostsBtn = document.getElementById('btnLoadPosts');
+    let selectPostsDropdown = document.getElementById('posts');
+    let viewPostsBtn = document.getElementById('btnViewPost');
+    
+    loadPostsBtn.addEventListener('click', loadPosts)
+    viewPostsBtn.addEventListener('click', viewPosts)
+    
+
+    async function loadPosts() {
+        let responce = await fetch(endpoints.posts);
+        let data = await responce.json();
+        try {
+            renderPosts(data)
+            loadPostsBtn.setAttribute('disabled', 'disabled')
+        } catch (error) {
+            // alert(error);
+            throw new Error(error);
+        }
+    }
+
+    async function getAllComments() {
+        let response = await fetch(endpoints.comments)
+        let data = response.json(response)
+        return data
+    }
+    
+    async function viewPosts() {
+        let postID = selectPostsDropdown.value;
+        
+        let allComments;
+
+        try {
+            allComments = await getAllComments()
+        } catch (error) {
+            throw new Error(error);
+        }
+
+        let commentsArr = [];
+
+        Object.values(allComments).forEach(obj => {
+            if (obj.postId === postID) {
+                commentsArr.push(obj)
+            }
+        });
+        
+        renderPostDetails();
+        renderComments(commentsArr);
+    }
+    
+    async function renderPosts(data) {
+        Object.values(data).forEach(obj => {
+            let option = document.createElement('option');
+
+            option.setAttribute('value', obj.id);
+            option.textContent = obj.title;
+
+            selectPostsDropdown.appendChild(option)
+        });
+    }
+
+    async function renderPostDetails() {
+        let titleElem = document.querySelector('h1#post-title')
+        let textElem = document.querySelector('p#post-body')
+
+        let id = selectPostsDropdown.value
+        let url = endpoints.posts + id
+        
+        let response = await fetch(url)
+        let data = await response.json(response)
+        
+        titleElem.textContent = data.title
+        textElem.textContent = data.body
+    }
+
+    function renderComments(commentsArr) {
+        let commentsUL = document.querySelector('ul#post-comments')
+        commentsUL.innerHTML = ''
+
+        commentsArr.forEach(obj => {
+            let li = document.createElement('li');
+            li.setAttribute('id', obj.id);
+            li.textContent = obj.text;
+
+            commentsUL.appendChild(li)
+        });
+    }
+    
 }
 
 attachEvents();
